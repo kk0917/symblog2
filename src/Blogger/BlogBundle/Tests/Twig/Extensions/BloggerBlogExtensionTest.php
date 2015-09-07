9ff426cec67b0c885531b6033e5ca2662a18556c
Kou Kamenashi <kamenashi0917@gmail.com>
2015-09-07 13:16:09 +0900

    symblogパート6：Twig エクステンションのテスト のテストコード記述及びテスト完了

--- src/Blogger/BlogBundle/Tests/Twig/Extensions/BloggerBlogExtensionTest.php
+++ src/Blogger/BlogBundle/Tests/Twig/Extensions/BloggerBlogExtensionTest.php
@@ -1,54 +1,31 @@
 <?php
-// src/Blogger/BlogBundle/Twig/Extensions/BloggerBlogExtension.php
+// src/Blogger/BlogBundle/Tests/Twig/Extensions/BloggerBlogExtensionTest.php
 
-namespace Blogger\BlogBundle\Twig\Extensions;
+namespace Blogger\BlogBundle\Tests\Twig\Extensions;
 
-class BloggerBlogExtension extends \Twig_Extension
+use Blogger\BlogBundle\Twig\Extensions\BloggerBlogExtension;
+
+class BloggerBlogExtensionTest extends \PHPUnit_Framework_TestCase
 {
-    public function getFilters()
+    public function testCreatedAgo()
     {
-        return array(
-            'created_ago' => new \Twig_Filter_Method($this, 'createdAgo'),
-        );
+        $blog = new BloggerBlogExtension();
+
+        $this->assertEquals("0 seconds ago", $blog->createdAgo(new \DateTime()));
+        $this->assertEquals("34 seconds ago", $blog->createdAgo($this->getDateTime(-34)));
+        $this->assertEquals("1 minute ago", $blog->createdAgo($this->getDateTime(-60)));
+        $this->assertEquals("2 minutes ago", $blog->createdAgo($this->getDateTime(-120)));
+        $this->assertEquals("1 hour ago", $blog->createdAgo($this->getDateTime(-3600)));
+        $this->assertEquals("1 hour ago", $blog->createdAgo($this->getDateTime(-3601)));
+        $this->assertEquals("2 hours ago", $blog->createdAgo($this->getDateTime(-7200)));
+
+        // Cannot create time in the future
+        $this->setExpectedException('\InvalidArgumentException');
+        $blog->createdAgo($this->getDateTime(60));
     }
 
-    public function createdAgo(\DateTime $dateTime)
+    protected function getDateTime($delta)
     {
-        $delta = time() - $dateTime->getTimestamp();
-        if ($delta < 0)
-            throw new \InvalidArgumentException("createdAgo is unable to handle dates in the future");
-
-        $duration = "";
-        if ($delta < 60)
-        {
-            // Seconds
-            $time = $delta;
-            $duration = $time . " second" . (($time > 1) ? "s" : "") . " ago";
-        }
-        else if ($delta <= 3600)
-        {
-            // Mins
-            $time = floor($delta / 60);
-            $duration = $time . " minute" . (($time > 1) ? "s" : "") . " ago";
-        }
-        else if ($delta <= 86400)
-        {
-            // Hours
-            $time = floor($delta / 3600);
-            $duration = $time . " hour" . (($time > 1) ? "s" : "") . " ago";
-        }
-        else
-        {
-            // Days
-            $time = floor($delta / 86400);
-            $duration = $time . " day" . (($time > 1) ? "s" : "") . " ago";
-        }
-
-        return $duration;
-    }
-
-    public function getName()
-    {
-        return 'blogger_blog_extension';
+        return new \DateTime(date("Y-m-d H:i:s", time()+$delta));
     }
 }
